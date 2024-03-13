3bf5262b63f44551952ae1ded7471cd0033d3a55
ickee953 <ickee953@gmail.com>
2024-03-13 20:14:54 +0300

    Change Maven dependencies for Spring Boot, create main application class.

---
+++ src/main/java/com/online/items/core/Application.java
@@ -0,0 +1,14 @@
+package com.online.items.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.online.booking.core")
+public class Application extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
