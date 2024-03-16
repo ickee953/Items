23a3e2cb9602a2434c4db2029737a75d1c3ed64c
ickee953 <ickee953@gmail.com>
2024-03-16 16:25:24 +0300

    Create web mvc config to serve static resources. Change package properties to *.war, change web sequrity config and login page. Move static resources to webapp.

--- src/main/java/com/online/items/core/WebSecurityConfig.java
+++ src/main/java/com/online/items/core/WebSecurityConfig.java
@@ -18,7 +18,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/assets/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
