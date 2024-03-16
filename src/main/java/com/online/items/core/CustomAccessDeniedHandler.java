60c34b25a5b39fb5b6a51c686f4f35d20ce5a356
ickee953 <ickee953@gmail.com>
2024-03-16 12:54:05 +0300

    Clean pom dependencies, remove javax.servlet.* packages from code.

--- src/main/java/com/online/items/core/CustomAccessDeniedHandler.java
+++ src/main/java/com/online/items/core/CustomAccessDeniedHandler.java
@@ -8,16 +8,9 @@
 
 package com.online.items.core;
 
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.logging.Logger;
 
 @Component
@@ -41,7 +34,7 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
         }
 
         response.sendRedirect(request.getContextPath() + "/login?error");
-    }*/
+    }
 
     @Override
     public void handle(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, jakarta.servlet.ServletException {
