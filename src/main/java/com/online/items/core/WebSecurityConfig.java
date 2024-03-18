5162c8960c711d58b4c0e7a22ef550e778969db0
ickee953 <ickee953@gmail.com>
2024-03-18 11:46:11 +0300

    Repackage back to *.jar standalone application, setup custom resource handlers and html templates resolver.

--- src/main/java/com/online/items/core/WebSecurityConfig.java
+++ src/main/java/com/online/items/core/WebSecurityConfig.java
@@ -18,7 +18,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/assets/**").permitAll()
+                        .requestMatchers("/css/**", "/fonts/**", "/js/**", "/scss/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
