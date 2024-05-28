10fd67d463941c41dcf7db85fa81d7a0ff590e48
ickee953 <ickee953@gmail.com>
2024-05-29 00:25:04 +0300

    Apply web security configuration for separate access to api by roles.

--- src/main/java/com/online/items/core/WebSecurityConfig.java
+++ src/main/java/com/online/items/core/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.online.items.core;
 
+import com.online.items.core.domain.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -17,9 +19,17 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/users", "/items", "/css/**", "/fonts/**", "/js/**", "/scss/**").permitAll()
-                        .anyRequest().authenticated()
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/items", "/css/**", "/fonts/**", "/js/**", "/scss/**").permitAll()
+                        .requestMatchers("/users").hasAnyRole(Role.ROLE_ADMINISTRATOR, Role.ROLE_USER)
+                        .requestMatchers(HttpMethod.GET, "/api/v1/users").hasAnyRole(Role.ROLE_ADMINISTRATOR, Role.ROLE_USER)
+                        .requestMatchers(HttpMethod.POST, "/api/v1/users").hasRole(Role.ROLE_ADMINISTRATOR)
+                        .requestMatchers(HttpMethod.PUT, "/api/v1/users/*").hasRole(Role.ROLE_ADMINISTRATOR)
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/users/*").hasRole(Role.ROLE_ADMINISTRATOR)
+                        .requestMatchers(HttpMethod.GET, "/api/v1/items").hasAnyRole(Role.ROLE_ADMINISTRATOR, Role.ROLE_USER)
+                        .requestMatchers(HttpMethod.POST, "/api/v1/items").hasAnyRole(Role.ROLE_ADMINISTRATOR, Role.ROLE_USER)
+                        .requestMatchers(HttpMethod.PUT, "/api/v1/items/*").hasAnyRole(Role.ROLE_ADMINISTRATOR, Role.ROLE_USER)
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/items/*").hasAnyRole(Role.ROLE_ADMINISTRATOR, Role.ROLE_USER)
                 )
                 .formLogin((form) -> form
                         .loginPage("/login")
@@ -32,14 +42,18 @@ public class WebSecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user =
+        return new InMemoryUserDetailsManager(
+                User.withDefaultPasswordEncoder()
+                        .username("admin")
+                        .password("password")
+                        .roles("ROLE_ADMINISTRATOR")
+                        .build(),
                 User.withDefaultPasswordEncoder()
                         .username("user")
                         .password("password")
-                        .roles("ADMINISTRATOR")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
+                        .roles("ROLE_USER")
+                        .build()
+        );
     }
 
 }
